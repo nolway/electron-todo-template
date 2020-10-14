34471e40a05802ccc99a85aa367859f55ac7f812
christophele <le_christophe@hotmail.fr>
2020-10-15 00:22:21 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron');
+
+function createWindow() {
+    let win = new BrowserWindow({
+        minWidth: 1200,
+        minHeight: 800,
+        maxWidth: 1500,
+        maxHeight: 1000,
+        frame: false,
+        show: false,
+        transparent: true,
+        closable: false,
+        resizable: false,
+        movable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('views/home/home.html');
+    win.once('ready-to-show', () => {
+        win.show();
+    });
+    
+    win.on('closed', () => {
+        win = null;
+    });
+
+    return win;
+}
+
+app.whenReady().then(createWindow);
