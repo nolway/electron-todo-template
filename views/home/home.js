ccc5988878d6033deef42bc4799f943fa5cac6e9
Kameliskrane <38702401+Kameliskrane@users.noreply.github.com>
2021-02-01 20:10:06 +0100

    Update home.js
    Double Call to addTask() And Bootstrap need JQuery for working

--- views/home/home.js
+++ views/home/home.js
@@ -103,7 +103,6 @@ var ajaxRequest=function(){
 }
 
 //Set the click handler to the addTask function.
-addButton.onclick=addTask;
 addButton.addEventListener("click",addTask);
 addButton.addEventListener("click",ajaxRequest);
 
